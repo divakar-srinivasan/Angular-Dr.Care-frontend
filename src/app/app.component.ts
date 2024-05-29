cdb187cb1022c391395a1aff16ed34f62623af6a
divakar <sdivakar2005@gmail.com>
2024-05-29 17:45:24 +0530

    Angular_project-1st-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HelloComponent } from './Components/hello.component';
+import { HeaderComponent } from './header/header.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,FormsModule,HttpClientModule  ,RouterLink,HelloComponent ,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Myproject';
+  title = 'Divakar';
 }
